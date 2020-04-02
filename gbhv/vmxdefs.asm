d825301398abf869964a0639de1fc2f0022abd7d
btbd <b4davis88@gmail.com>
2020-04-02 05:47:56 -0400

    Fix ASM formatting

--- gbhv/vmxdefs.asm
+++ gbhv/vmxdefs.asm
@@ -127,11 +127,11 @@ HvEnterFromGuest PROC
 	; or a #GP exception is generated.
 	sub rsp, 68h
 	movaps xmmword ptr [rsp], xmm0
-	movaps xmmword ptr [rsp + 10h], xmm1
-	movaps xmmword ptr [rsp + 20h], xmm2
-	movaps xmmword ptr [rsp + 30h], xmm3
-	movaps xmmword ptr [rsp + 40h], xmm4
-	movaps xmmword ptr [rsp + 50h], xmm5
+	movaps xmmword ptr [rsp+10h], xmm1
+	movaps xmmword ptr [rsp+20h], xmm2
+	movaps xmmword ptr [rsp+30h], xmm3
+	movaps xmmword ptr [rsp+40h], xmm4
+	movaps xmmword ptr [rsp+50h], xmm5
 
 	; Call HvHandleVmExit to actually handle the
 	; incoming exit
@@ -139,13 +139,13 @@ HvEnterFromGuest PROC
 	call HvHandleVmExit
 	add rsp, 20h
 
-	; Restore SSE registers
+	; Restore XMM registers
 	movaps xmm0, xmmword ptr [rsp]
-	movaps xmm1, xmmword ptr [rsp + 10h]
-	movaps xmm2, xmmword ptr [rsp + 20h]
-	movaps xmm3, xmmword ptr [rsp + 30h]
-	movaps xmm4, xmmword ptr [rsp + 40h]
-	movaps xmm5, xmmword ptr [rsp + 50h]
+	movaps xmm1, xmmword ptr [rsp+10h]
+	movaps xmm2, xmmword ptr [rsp+20h]
+	movaps xmm3, xmmword ptr [rsp+30h]
+	movaps xmm4, xmmword ptr [rsp+40h]
+	movaps xmm5, xmmword ptr [rsp+50h]
 	add rsp, 68h
 
 	; If it's not successful, we need to stop and figure out why
